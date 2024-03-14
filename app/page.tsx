a815cefdf8e06b00f926a8a7ef3f1abc30babc03
kishan0779 <124876418+kishan0779@users.noreply.github.com>
2024-03-14 14:15:16 +0530

    full com

--- app/page.tsx
+++ app/page.tsx
@@ -1,58 +1,58 @@
-
 "use client"
 import React, { useEffect } from 'react'
 import HeroSection from './Components/HeroSection/HeroSection'
 import Section from './Components/Section/Section'
-
 import BgOverview from '@/public/bg-overview.jpeg'
 import BgInterior from '@/public/bg-interior.jpeg'
 import BgCharging from '@/public/bg-charging.jpeg'
 import BgConnectivity from '@/public/bg-connectivity.jpeg'
 import BgSafety from '@/public/bg-safety.jpeg'
-import Lenis from  "@studio-freight/lenis";
+import Lenis from "@studio-freight/lenis";
 
 const page = () => {
-  useEffect(() => {
-    const lenis = new Lenis();
-    function raf(time: any){
-      lenis.raf(time);
+    useEffect(() => {
+      const lenis = new Lenis();
+      function raf(time: any) {
+        lenis.raf(time);
+        requestAnimationFrame(raf);
+      }
       requestAnimationFrame(raf);
-    }
-     requestAnimationFrame(raf);
-    
-  },[]);
+    }, []);
+  
+
   return (
+   
     <div>
-      <HeroSection/>
-      <Section 
-      image={BgOverview}
-      tag='Overview'
-      title='Elevate adventure in the luxury electric SUV of tomorrow'
-      description='More space. Elevate adventure in the luxury electric SUV of tomorrowElevate adventure in the luxury electric SUV of tomorrow '
+      <HeroSection />
+      <Section
+        image={BgOverview}
+        tag='Overview'
+        title='Elevate adventure in the luxury electric SUV of tomorrow'
+        description='More space. Elevate adventure in the luxury electric SUV of tomorrowElevate adventure in the luxury electric SUV of tomorrow '
       />
-      <Section 
-      image={BgInterior}
-      tag='Interior'
-      title='Interior space. superrior taste.'
-      description='More space. Elevate adventure in the luxury electric SUV of tomorrowElevate adventure in the luxury electric SUV of tomorrow '
+      <Section
+        image={BgInterior}
+        tag='Interior'
+        title='Interior space. superrior taste.'
+        description='More space. Elevate adventure in the luxury electric SUV of tomorrowElevate adventure in the luxury electric SUV of tomorrow '
       />
-      <Section 
-      image={BgCharging}
-      tag='Charging'
-      title='Lond-distance in lightning speed.'
-      description='More space. Elevate adventure in the luxury electric SUV of tomorrowElevate adventure in the luxury electric SUV of tomorrow '
+      <Section
+        image={BgCharging}
+        tag='Charging'
+        title='Lond-distance in lightning speed.'
+        description='More space. Elevate adventure in the luxury electric SUV of tomorrowElevate adventure in the luxury electric SUV of tomorrow '
       />
-      <Section 
-      image={BgConnectivity}
-      tag='Connectivity'
-      title='Feel the world at your fingertips'
-      description='More space. Elevate adventure in the luxury electric SUV of tomorrowElevate adventure in the luxury electric SUV of tomorrow '
+      <Section
+        image={BgConnectivity}
+        tag='Connectivity'
+        title='Feel the world at your fingertips'
+        description='More space. Elevate adventure in the luxury electric SUV of tomorrowElevate adventure in the luxury electric SUV of tomorrow '
       />
-      <Section 
-      image={BgSafety}
-      tag='Safety'
-      title='Designed for peace of mind'
-      description='More space. Elevate adventure in the luxury electric SUV of tomorrowElevate adventure in the luxury electric SUV of tomorrow '
+      <Section
+        image={BgSafety}
+        tag='Safety'
+        title='Designed for peace of mind'
+        description='More space. Elevate adventure in the luxury electric SUV of tomorrowElevate adventure in the luxury electric SUV of tomorrow '
       />
     </div>
   )
